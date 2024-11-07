52b503305dfac9afdd0e65ed9b5b17e3f1a0c51f
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2024-11-07 18:42:33 +0100

    a

--- src/index.php
+++ src/index.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html>
-    <head>
-
-    </head>
-    <body>
-        <h1>Test</h1>
-        <?php 
-            echo "<p>Hello world!<p>"
-        ?>
-    </body>
-</html>
+<?php phpinfo() ?>
