5c2129852936178ff1dcf0d02476e05181537a29
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2024-11-14 15:16:49 +0100

    fix vari

--- src/connessione.php
+++ src/connessione.php
@@ -1,20 +1,16 @@
 <?php 
 try{
-    //!DA CAMBIARE PROTOCOLLO E HOST
-    //!DA CAMBIARE PROTOCOLLO E HOST
-    //!DA CAMBIARE PROTOCOLLO E HOST
-    //!DA CAMBIARE PROTOCOLLO E HOST
-    $pdo = new PDO('pgsql:dbname=tlazzari host=host.docker.internal port=3306', 'tlazzari', 'Aix5Booyeepohfee'); 
+    $pdo = new PDO('mysql:dbname=tlazzari; host=db; port=3306', 'root'); 
     // Set PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Define and execute the query
-    $query = "SELECT * FROM t";
+    $query = "SELECT * FROM a";
     $stmt = $pdo->query($query);
 
     // Fetch and print each row
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo $row['at'] . "<br>"; // Replace 'column_name' with the actual column name of your table
+        echo $row['id'] . "<br>"; // Replace 'column_name' with the actual column name of your table
     }
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
