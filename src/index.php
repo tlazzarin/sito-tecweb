cd83f859c9fd74fb5ed3ec91a455778c985da107
Yuri <lunardonyuri@icloud.com>
2025-01-11 13:20:05 +0100

    homepage e altri fix

--- src/index.php
+++ src/index.php
@@ -1,15 +1,12 @@
 <?php
-
+require_once("DB/database.php");
+use DB\Functions;
 session_start();
 
-use DB\Functions;
-
-// Include i file necessari
-require_once("DB/database.php");
-require_once "grafica.php";
-
-// Carica la pagina HTML di base
 $paginaHTML = file_get_contents("index.html");
+
+$connessione = new Functions();
+$checkConnection = $connessione->openConnection();
 
 if(isset($_SESSION['Username'])){
     if(!isset($_SESSION['isAdmin']))
@@ -44,6 +41,36 @@ else
     </section>";
 }
 
+
+$PercorsiTop = "";
+
+if($checkConnection){
+    $queryPercorsiTop = $connessione->get_percorsi_top();
+    
+    if($queryPercorsiTop->ok() && !$queryPercorsiTop->is_empty()) {
+        $PercorsiTop = "<div class=\"percorsi-top-container\">"; 
+        
+        foreach($queryPercorsiTop->get_result() as $percorso){
+            
+            $PercorsiTop .= "<a href=\"percorso.php?id=" . $percorso['id'] . "\">
+                <section class=\"carta\">
+                    <img src=\"./assets/img/percorsi/".$percorso['id_immagine']."\" alt=\"".$percorso['alt']."\">
+                    <h3 class=\"link-percorso\">".$percorso['titolo']."</h3>                
+                </section>
+            </a>";
+        }
+        
+        $PercorsiTop .= "</div>";  
+    }
+    
+    
+    $connessione->closeConnection();
+} else {
+    $_SESSION["error"] = "Impossibile connettersi al sistema";
+}
+
+
+$paginaHTML = str_replace("[percorsi_top]", $PercorsiTop, $paginaHTML);
 $paginaHTML =str_replace("[prima_opzione]",$prima_opzione,$paginaHTML);
 $paginaHTML =str_replace("[seconda_opzione]",$seconda_opzione,$paginaHTML);
 
