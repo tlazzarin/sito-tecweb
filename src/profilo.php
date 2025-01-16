f220000ee0e671e6b1c706354662642b81590050
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2025-01-16 14:23:54 +0100

    feat: aggiunto scheletro pagine amministrazione e profilo

---
+++ src/profilo.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+if(!isset($_SESSION["Username"]))
+    header("Location: error/401.php");
+if($_SESSION["isAdmin"]==1)
+    header("Location: index.php");
+
+$paginaHtml = file_get_contents("profilo.html");
+
+$paginaHtml = str_replace("[username]", $_SESSION["Username"], $paginaHtml);
+echo $paginaHtml;
+
+?>
