4d4aec08e10399682e4a99d194e1a79a8a495245
Gaia Pistori <gaia.pistori@gmail.com>
2025-01-31 09:51:48 +0100

    [fix] navbar errori

--- src/generate_navbar.php
+++ src/generate_navbar.php
@@ -6,23 +6,42 @@ function generateNavbar($session_data)
     //Caso utente loggato [profilo/admin  |  Logout]
     if (isset($session_data['Username'])) {
         if ($session_data['isAdmin'] == 1)
-            $risposta[0] = "<a href=\"/pannelloAmministrazione.php\">Pannello Amministrazione</a>";
+            $risposta[0] = "<a href=\"./pannelloAmministrazione.php\">Pannello Amministrazione</a>";
         else
-            $risposta[0] = "<a href=\"/profilo.php\">Profilo</a>";
+            $risposta[0] = "<a href=\"./profilo.php\">Profilo</a>";
         $risposta[1] = "
-                <a href=\"/logout.php\" class=\"action-button\">
-                    Logout <img src=\"/assets/right-to-bracket-solid.svg\" alt=\"Icona logout\" class=\"icon-logout\">
+                <a href=\"./logout.php\" class=\"action-button\">
+                    Logout <img src=\"./assets/right-to-bracket-solid.svg\" alt=\"Icona logout\" class=\"icon-logout\">
                 </a>";
     } else { //Caso utente non loggato [Registrati | Accedi]
-        $risposta[0] = "<a href=\"/registrati.php\">Registrati</a>";
+        $risposta[0] = "<a href=\"./registrati.php\">Registrati</a>";
         $risposta[1] = "
-                <a href=\"/accedi.php\" class=\"action-button\">
-                    Accedi <img src=\"/assets/right-from-bracket-solid.svg\" alt=\"Icona accedi\" class=\"icon-accedi\">
+                <a href=\"./accedi.php\" class=\"action-button\">
+                    Accedi <img src=\"./assets/right-from-bracket-solid.svg\" alt=\"Icona accedi\" class=\"icon-accedi\">
                 </a>";
+    }
+    return $risposta;
+}
 
-
-
-
+function generateNavbarError($session_data)
+{
+    $risposta = array();
+    //Caso utente loggato [profilo/admin  |  Logout]
+    if (isset($session_data['Username'])) {
+        if ($session_data['isAdmin'] == 1)
+            $risposta[0] = "<a href=\"../pannelloAmministrazione.php\">Pannello Amministrazione</a>";
+        else
+            $risposta[0] = "<a href=\"../profilo.php\">Profilo</a>";
+        $risposta[1] = "
+                <a href=\"../logout.php\" class=\"action-button\">
+                    Logout <img src=\"../assets/right-to-bracket-solid.svg\" alt=\"Icona logout\" class=\"icon-logout\">
+                </a>";
+    } else { //Caso utente non loggato [Registrati | Accedi]
+        $risposta[0] = "<a href=\"../registrati.php\">Registrati</a>";
+        $risposta[1] = "
+                <a href=\"../accedi.php\" class=\"action-button\">
+                    Accedi <img src=\"../assets/right-from-bracket-solid.svg\" alt=\"Icona accedi\" class=\"icon-accedi\">
+                </a>";
     }
     return $risposta;
 }
